dd1f0dfd333633296009e48053cf83824307e5c5
Donor <donorfelita@msn.com>
2023-12-06 17:13:21 +0100

    Delete tag

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,15 +17,14 @@ resource "google_cloudbuild_trigger" "resume-backend-trigger" {
   ignored_files = [".gitignore", "terraform/*"]
 }
 
+resource "google_cloud_run_v2_service" "api" {
+  name     = "api"
+  location = "europe-west9"
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
-# resource "google_cloud_run_v2_service" "api" {
-#   name     = "cloudrun-api"
-#   location = "europe-west9"
-#   ingress  = "INGRESS_TRAFFIC_ALL"
-
-#   template {
-#     containers {
-#       image = ""
-#     }
-#   }
-# }
+  template {
+    containers {
+      image = "europe-west9-docker.pkg.dev/resume-404711/cloud-run-resume/cloud-run-backend-api"
+    }
+  }
+}
