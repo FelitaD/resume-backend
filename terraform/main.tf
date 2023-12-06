5cc0d0d1139dcec35eb5c68d9b3b276168a0a739
Donor <donorfelita@msn.com>
2023-12-06 15:49:10 +0100

    Add terraform files

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+resource "google_artifact_registry_repository" "cloud-run-images" {
+  location      = "europe-west9"
+  repository_id = "cloud-run-images-resume"
+  description   = "resume challenge images deployed to cloud run"
+  format        = "DOCKER"
+}
+
+resource "google_cloudbuild_trigger" "resume-backend-trigger" {
+  github {
+    owner = "FelitaD"
+    name  = "resume-backend"
+    push {
+       branch = "main"
+      }
+  }
+  filename      = "terraform/cloudbuild-cloud-run-api.yaml"
+  ignored_files = [".gitignore", "terraform/*"]
+}
+
+# resource "google_cloud_run_v2_service" "api" {
+#   name     = "cloudrun-api"
+#   location = "europe-west9"
+#   ingress  = "INGRESS_TRAFFIC_ALL"
+
+#   template {
+#     containers {
+#       image = ""
+#     }
+#   }
+# }
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project" {}
+
+variable "credentials_file" {}
+
+variable "region" {
+  default = "europe-west2"
+}
+
+variable "zone" {
+  default = "europe-west2-c"
+}
