90b48b5599f0ae965e2bf178662e823799cfbcc7
Donor <donorfelita@msn.com>
2023-12-06 20:47:05 +0100

    Put region variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "google_artifact_registry_repository" "cloud-run-images" {
-  location      = "europe-west9"
+  location      = var.region
   repository_id = "cloud-run-resume"
   description   = "resume challenge images deployed to cloud run"
   format        = "DOCKER"
@@ -19,7 +19,7 @@ resource "google_cloudbuild_trigger" "resume-backend-trigger" {
 
 resource "google_cloud_run_v2_service" "api" {
   name     = "api"
-  location = "europe-west9"
+  location = var.region
   ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
@@ -27,4 +27,14 @@ resource "google_cloud_run_v2_service" "api" {
       image = "europe-west9-docker.pkg.dev/resume-404711/cloud-run-resume/cloud-run-backend-api"
     }
   }
+}
+
+resource "google_cloud_run_v2_service_iam_binding" "binding" {
+  project = var.project
+  location = var.region
+  name = "allow_unauthenticated_users"
+  role = "roles/run.invoker"
+  members = [
+    "allUsers",
+  ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,9 +3,9 @@ variable "project" {}
 variable "credentials_file" {}
 
 variable "region" {
-  default = "europe-west2"
+  default = "europe-west9"
 }
 
 variable "zone" {
-  default = "europe-west2-c"
+  default = "europe-west9-c"
 }
