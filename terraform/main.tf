17bec3a5cfd9b5486a26a0ed054364b657e432ec
Donor <donorfelita@msn.com>
2024-01-23 14:01:44 +0100

    Rename resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,17 @@
-resource "google_artifact_registry_repository" "cloud-run-images" {
+# API backend
+
+# Create Artifact repository to store API image
+
+resource "google_artifact_registry_repository" "default" {
   location      = var.region
-  repository_id = "cloud-run-resume"
-  description   = "resume challenge images deployed to cloud run"
+  repository_id = "api-registry"
+  description   = "Registry containing API image to deploy to Cloud Run"
   format        = "DOCKER"
 }
 
-resource "google_cloudbuild_trigger" "resume-backend-trigger" {
+# Create Cloud Build trigger to build and push image to Artifact Registry when push on main branch
+
+resource "google_cloudbuild_trigger" "default" {
   github {
     owner = "FelitaD"
     name  = "resume-backend"
@@ -13,9 +19,11 @@ resource "google_cloudbuild_trigger" "resume-backend-trigger" {
        branch = "main"
       }
   }
-  filename      = "terraform/cloudbuild-cloud-run-api.yaml"
+  filename      = "terraform/cloudbuild_api.yaml"
   ignored_files = [".gitignore", "terraform/*"]
 }
+
+# Deploy API backend on Cloud Run
 
 resource "google_cloud_run_v2_service" "api" {
   name     = "api"
@@ -24,17 +32,77 @@ resource "google_cloud_run_v2_service" "api" {
 
   template {
     containers {
-      image = "europe-west9-docker.pkg.dev/resume-404711/cloud-run-resume/cloud-run-backend-api"
+      image = "europe-west9-docker.pkg.dev/resume-404711/api-registry/api_image"
     }
   }
 }
 
-resource "google_cloud_run_v2_service_iam_binding" "binding" {
-  project = var.project
-  location = var.region
-  name = "allow_unauthenticated_users"
+# Allow unauthenticated requests on API backend
+
+resource "google_cloud_run_v2_service_iam_member" "default" {
+  project = google_cloud_run_v2_service.api.project
+  location = google_cloud_run_v2_service.api.location
+  name = google_cloud_run_v2_service.api.name
   role = "roles/run.invoker"
-  members = [
-    "allUsers",
-  ]
+  member = "allUsers"
+}
+
+# Gateway
+
+# Reserve hostname for Gateway on Cloud Run
+
+# resource "google_cloud_run_v2_service" "gateway" {
+#   name     = "gateway"
+#   location = var.region
+#   ingress  = "INGRESS_TRAFFIC_ALL"
+
+#   template {
+#     containers {
+#       image = "gcr.io/cloudrun/hello"
+#     }
+#   }
+# }
+
+# Allow unauthenticated requests on Gateway
+
+# resource "google_cloud_run_v2_service_iam_member" "gateway" {
+#   project = google_cloud_run_v2_service.gateway.project
+#   location = google_cloud_run_v2_service.gateway.location
+#   name = google_cloud_run_v2_service.gateway.name
+#   role = "roles/run.invoker"
+#   member = "allUsers"
+# }
+
+# Create Cloud Endpoints endpoint for Gateway service
+
+resource "google_endpoints_service" "default" {
+  service_name   = "gateway-jq4t4jadfq-od.a.run.app"
+  project        = var.project
+  openapi_config = file("/Users/donor/Code/resume-challenge/resume-backend/gateway/openapi-run.yaml")
+}
+
+# [Manual step] Build ESPv2 image from script
+
+# Deploy Gateway revision (ESPv2) on Cloud Run
+
+resource "google_cloud_run_v2_service" "gateway-espv2" {
+  name     = "gateway"
+  location = var.region
+  ingress  = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    containers {
+      image = "eu.gcr.io/resume-404711/endpoints-runtime-serverless:2.46.0-gateway-jq4t4jadfq-od.a.run.app-2024-01-02r0"
+    }
+  }
+}
+
+# [Useless ?] Allow unauthenticated requests on Gateway revision
+
+resource "google_cloud_run_v2_service_iam_member" "gateway-espv2" {
+  project = google_cloud_run_v2_service.gateway-espv2.project
+  location = google_cloud_run_v2_service.gateway-espv2.location
+  name = google_cloud_run_v2_service.gateway-espv2.name
+  role = "roles/run.invoker"
+  member = "allUsers"
 }
