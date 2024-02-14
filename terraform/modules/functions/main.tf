3a91bf6c23bf3049910750f58fbf08b43376a96e
Donor <donorfelita@msn.com>
2024-02-14 18:13:59 +0100

    Modify GCF location allowed for 1st and 2nd gen

--- terraform/modules/functions/main.tf
+++ terraform/modules/functions/main.tf
@@ -18,7 +18,7 @@ resource "google_storage_bucket_object" "object" {
 
 resource "google_cloudfunctions2_function" "default" {
   name        = "function-v2"
-  location    = "europe-west2"
+  location    = "europe-west1"
   description = "Increment Firestore visitor count"
 
   build_config {
